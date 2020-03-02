93f9fe30cbf02afffc777b51171df05ada555bb8
Natalia Bryzgalova <bryzgalova_n@mail.ru>
2020-03-03 00:51:39 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        double summTicket = 48_049;
+        double milenumber = summTicket/20;
+          System.out.println(milenumber);
+    }
+}
