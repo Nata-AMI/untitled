7960a5a0b40548eeeaaffcfe6ca4646122b87ee3
Natalia Bryzgalova <bryzgalova_n@mail.ru>
2020-03-03 19:44:09 +0500

    Вырвнивание кода

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         double summTicket = 48_049;
         double mileNumber = summTicket/20;
-          System.out.println(mileNumber);
+        System.out.println(mileNumber);
     }
 }
