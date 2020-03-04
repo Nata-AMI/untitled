b7321200440ca74991676ab0d09ac8a6ef72dbfd
Natalia Bryzgalova <bryzgalova_n@mail.ru>
2020-03-04 09:33:17 +0500

    Добавление в программу Servise метода

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        double summTicket = 48_049;
-        double mileNumber = summTicket/20;
-        System.out.println(mileNumber);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
